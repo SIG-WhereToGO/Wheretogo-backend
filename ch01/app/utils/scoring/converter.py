1d0b471da3d159e84a9f35affad89f4352cb8286
윤나경 <yunnagyeong7@gmail.com>
2026-08-28 17:59:47 +0900

    chore: 타입 오류 수정

--- ch01/app/utils/scoring/converter.py
+++ ch01/app/utils/scoring/converter.py
@@ -19,10 +19,11 @@ def recommendation_candidate_converter(
     tourist_id = scoring_model.tourist_id
     similarity = scoring_model.similarity
 
-    tourist_style_tagDic = scoring_model.tourist_style_tags
-    user_style_tagDic = user_style_tags
-    tourist_style_tagTuple = convert_to_Tuple(standard, tourist_style_tagDic)
-    user_style_tagTuple = convert_to_Tuple(standard, user_style_tagDic)
+    # scoring_model.tourist_style_tags는 SpotCandidateResponseFromData.from_db()에서
+    # 이미 TOURIST_STYLE_TAGS 순서의 tuple로 변환되어 들어옵니다. dict가 아니므로
+    # convert_to_Tuple()을 다시 적용하면 안 됩니다 (AttributeError 원인이었음).
+    tourist_style_tagTuple = scoring_model.tourist_style_tags
+    user_style_tagTuple = convert_to_Tuple(standard, user_style_tags)
 
     soft_f1 = calculate_soft_f1 (
         user_style_tags = user_style_tagTuple,
